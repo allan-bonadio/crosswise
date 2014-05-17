5b7db3e24128af1854454534d9eb58b52781ae57
Allan Bonadio <unsure@tactileint.com>
2014-05-17 14:21:55 -0700

    remove old ones, fix new ones

--- wikiDefs.sh
+++ wikiDefs.sh
@@ -10,36 +10,37 @@
 
 [ -d $wROOT/skins/common ] || echo "\n --- No wROOT set ---\n"
 
+# the root
+alias wwr='cd $wCW'
+
+# root of the mediawiki source tree
 alias ww='cd $wROOT'
+
+# the aux directory
+alias wwa='cd $wAUX'
 
 #alias wwbin='cd $wROOT/bin'
 #alias wwcon='cd $wROOT/config'
 alias wwdoc='cd $wROOT/docs'
-alias wwext='cd $wROOT/extensions'
 alias   wwe='cd $wROOT/extensions'
-alias   wwef='cd $wROOT/extensions/featSrc'
+alias  wwef='cd $wROOT/extensions/featSrc'
 #alias wwima='cd $wROOT/images'
-alias wwinc='cd $wROOT/includes'
 alias   wwi='cd $wROOT/includes'
 #alias wwlan='cd $wROOT/languages'
-alias wwmai='cd $wROOT/maintenance'
 #alias wwmat='cd $wROOT/math'
 #alias wwser='cd $wROOT/serialized'
 
 alias wwm='cd $wROOT/maintenance'
 
-alias wwski='cd $wROOT/skins'
 alias wws='cd $wROOT/skins'
 alias wwsc='cd $wROOT/skins/crosswise'
 
 #alias wwtes='cd $wROOT/tests'
 
-# the aux directory
-alias wwaux='cd $wAUX'
-alias wwa='cd $wAUX'
 
 
-# move the wROOT dir to here
-alias hereww='[ -d skins/common ] && export wROOT=$PWD; cd $wROOT; pwd '
+## move the wROOT dir to the current directory
+#alias hereww='[ -d skins/common ] && export wROOT=$PWD; cd $wROOT; pwd '
+
 export PATH="$PATH:$wAUX"
 
